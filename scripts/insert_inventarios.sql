9cac5d3c65bfd997d92cb408c8ddc57e06c61ee4
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2016-09-26 15:51:55 -0500

    se mejora

--- scripts/insert_inventarios.sql
+++ scripts/insert_inventarios.sql
@@ -1,12 +1,21 @@
+
 insert into INVENTARIOS (no_control, contacto_creador_id, descripcion_interna, vigencia_inicio, vigencia_fin) 
-select  trim(s.SERI), s.ID_CLIENTE, s.CONTACTO,  
-cast( '20'|| substring(s.FECHA_DESDE from 7 for 2 )|| '-' || substring(s.FECHA_DESDE from 4 for 2 )|| '-'||  substring(s.FECHA_DESDE from 1 for 2 ) as date ),
+select  trim(s.SERI), s.ID_CLIENTE
+,(trim(s.SERI)|| ' / ' ||replace(TRIM(S.MODELO),'Ê','')|| ' / ' ||  s.FECHA_VENTA || ' / ' || replace(S.GARANTIA_POLIZA, 'Garant’a','Garantía') || ' / ' || 
+replace(replace(replace(REPLACE(TRIM(S.CONTACTO),'„','Ñ'),'’','í'),'‡','á'),'—','ó')|| ' / ' || s.CONTACTO_ID )  
+
+,cast( '20'|| substring(s.FECHA_DESDE from 7 for 2 )|| '-' || substring(s.FECHA_DESDE from 4 for 2 )|| '-'||  substring(s.FECHA_DESDE from 1 for 2 ) as date ),
 cast( '20'|| substring(s.FECHA_HASTA from 7 for 2 )|| '-' || substring(s.FECHA_HASTA from 4 for 2 )|| '-'||  substring(s.FECHA_HASTA from 1 for 2 ) as date )
 from SERIES_APARATOS s where s.FECHA_DESDE <>'';
 
 
 insert into INVENTARIOS (no_control, contacto_creador_id, descripcion_interna, vigencia_inicio, vigencia_fin) 
-select  trim(s.SERI), s.ID_CLIENTE, s.CONTACTO,  
+select  trim(s.SERI), s.ID_CLIENTE
+,(trim(s.SERI)|| ' / ' ||replace(TRIM(S.MODELO),'Ê','')|| ' / ' ||  s.FECHA_VENTA || ' / ' || replace(S.GARANTIA_POLIZA, 'Garant’a','Garantía') || ' / ' || 
+replace(replace(replace(REPLACE(TRIM(S.CONTACTO),'„','Ñ'),'’','í'),'‡','á'),'—','ó')|| ' / ' || s.CONTACTO_ID )  
+
+,  
 null,null
 from SERIES_APARATOS s where s.FECHA_DESDE ='';
+
 
