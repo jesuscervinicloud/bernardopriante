e33cc1db75e42a83efdd3d4495baaa02b923ef09
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2016-10-11 13:06:12 -0500

    initial

---
+++ scripts/tarea4/insert_settings.sql
@@ -0,0 +1,26 @@
+	INSERT INTO SETTINGS
+	(ID, KEY, "VALUE")
+VALUES 
+	(1, 'path_bd_zeus', '/Users/alfredo/bernardopriante/bd/zeusaudimed.fdb');
+	
+	
+	
+INSERT INTO SETTINGS
+	(ID, KEY, "VALUE")
+VALUES 
+	(2, 'path_bd_microsip', '/Users/alfredo/bernardopriante/bd/audimed.fdb');
+	
+		INSERT INTO SETTINGS
+	(ID, KEY, "VALUE")
+VALUES 
+	(3, 'username', 'sysdba');
+	
+	
+	
+INSERT INTO SETTINGS
+	(ID, KEY, "VALUE")
+VALUES 
+	(4, 'password', 'masterkey');
+	
+	
+
