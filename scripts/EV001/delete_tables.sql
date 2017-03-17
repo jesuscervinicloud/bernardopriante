f3d078bdd5e9a4945eec017c2f6d044e6cbcf11c
jesus alfredo vazquez cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2017-03-17 10:47:53 -0600

    se agrega nuevo script para eliminar tablas, se modifica el scrip para eliminacion de llamdas porque no estaba funcionando bien, faltaba cubrir bien el caso cuando ya no hay mas llamadas

---
+++ scripts/EV001/delete_tables.sql
@@ -0,0 +1,11 @@
+delete from INVENTARIOS_CLIENTES;
+select count(*) from INVENTARIOS_CLIENTES;
+
+delete from INVENTARIOS;
+select count(*) from INVENTARIOS;
+
+delete from LIBRES_INVENTARIOS;
+select count(*) from libres_inventarios;
+
+delete from POLITICAS_DSCTOS;
+select * from POLITICAS_DSCTOS;
