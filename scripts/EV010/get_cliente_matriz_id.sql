8f94622f3c8e4cb2456a60a3cf85ce363ced4e8b
Jesus Alfredo Vazquez Cervin <jesus.alfredo.vazquez.cervin@gmail.com>
2017-06-03 09:57:57 -0500

    nuevamente se actualiza por el verdadero

--- scripts/EV010/get_cliente_matriz_id.sql
+++ scripts/EV010/get_cliente_matriz_id.sql
@@ -1,21 +1,23 @@
-CREATE OR ALTER PROCEDURE UPDATE_NOTAS_CLIENTES RETURNS (V_CLIENTE_ID INTEGER, V_NOMBRE VARCHAR(100))
-AS DECLARE lcComando varchar(5000);
+CREATE OR ALTER PROCEDURE GET_CLIENTE_MATRIZ_ID (P_CLIENTE_ID INTEGER)
+ RETURNS (P_CLIENTE_MATRIZ_ID INTEGER)
+AS declare variable q_select varchar(1000);
+declare variable path_bd_zeus varchar(1000);
+declare variable username varchar(50);
+declare variable password varchar(50);
 
 BEGIN
-      lcComando = 'select c.cliente_id from CLIENTES c';
-      
-FOR EXECUTE STATEMENT lcComando
-      
-into :V_CLIENTE_ID
-   DO BEGIN
-   V_NOMBRE = null;
-   select d.NOMBRE from CLIENTES d where d.CLIENTE_ID = (select first 1 P_CLIENTE_MATRIZ_ID from GET_CLIENTE_MATRIZ_ID(:V_CLIENTE_ID)) into :V_NOMBRE;
---   if (:V_NOMBRE is not null) then 
-      --V_NOMBRE = 'not nulo';
-      update CLIENTES c set c.NOTAS = coalesce(:V_NOMBRE, 'nulo' )  || ' ' || coalesce(c.NOTAS, '' ) where c.CLIENTE_ID = :V_CLIENTE_ID;
-     
-   SUSPEND;
-   END
-
- END
-
+--select c.CLIENTE_ID, c.CLIENTE_MATRIZ_ID from CLIENTES_COMPLEMENTOS c;
+q_select = 'select c.CLIENTE_MATRIZ_ID FROM CLIENTES_COMPLEMENTOS c where c.CLIENTE_ID = ' ||   P_CLIENTE_ID   ;                
+    
+    select s."VALUE" from SETTINGS s where s.KEY = 'path_bd_zeus' into :path_bd_zeus ;
+    select s."VALUE" from SETTINGS s where s.KEY = 'username' into :username ;
+    select s."VALUE" from SETTINGS s where s.KEY = 'password' into :password ;
+       
+       EXECUTE STATEMENT q_select
+       ON EXTERNAL DATA SOURCE path_bd_zeus
+       AS USER username
+       PASSWORD password 
+       WITH COMMON TRANSACTION
+       into :P_CLIENTE_MATRIZ_ID;
+       suspend;
+END
